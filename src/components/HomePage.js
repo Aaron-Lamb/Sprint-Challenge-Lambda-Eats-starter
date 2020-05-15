718d81194c99516ce5f05d538f3cb06a83c0efd5
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-15 16:34:12 -0700

    Moved the assets folder inside the src folder, added the pizza.jpg file to HomePage, and wrapped the app component in a Router in the index file.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Pizza from '../../Assets/Pizza.jpg';
+import Pizza from '../Assets/Pizza.jpg';
 
 const HomePage = () => {
     return(
